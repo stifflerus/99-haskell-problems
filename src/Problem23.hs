92a54ade7f7d34dd5c0f08b9dae503abb3ea8e2e
Robert Stiffler <robert@stiffler.us>
2018-08-06 23:22:21 -0400

    Renamed var

--- src/Problem23.hs
+++ src/Problem23.hs
@@ -9,6 +9,6 @@ rndSelect :: (Num b, Eq b) => [a] -> b -> IO [a]
 rndSelect _ 0 = return []
 rndSelect xs n = do
   i <- randomRIO (0, length xs - 1)
-  is <- rndSelect xs (n - 1)
-  return $ (xs !! i) : is
+  tail <- rndSelect xs (n - 1)
+  return $ (xs !! i) : tail
 
